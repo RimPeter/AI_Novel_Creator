afce8069fbe89112ae68ff9977e2163d2855982c
RimPeter <hull@localappliancerentals.co.uk>
2026-01-05 22:09:57 +0000

    Create base.html

--- novel_creator/settings.py
+++ novel_creator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a@59m%nsxzedimgx*61t!@#%pdnv=+4u3!fv@%r1a!p*tm0ipe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8010','local','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [

e9bc3b3e3f70fe7db0f18e8cbcd0b4a11dbe4cd0
RimPeter <hull@localappliancerentals.co.uk>
2026-01-06 09:00:34 +0000

    Create project htmls

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
 from . import views
+from .views import (
+    ProjectListView,
+    ProjectDetailView,
+    ProjectCreateView,
+    ProjectUpdateView,
+)
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("projects/", ProjectListView.as_view(), name="project-list"),
+    path("projects/new/", ProjectCreateView.as_view(), name="project-create"),
+    path("projects/<slug:slug>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("projects/<slug:slug>/edit/", ProjectUpdateView.as_view(), name="project-edit"),
 ]
-
